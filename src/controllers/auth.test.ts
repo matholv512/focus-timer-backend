f678316252a2e3858ede9578c40b02d1a961b34e
Matheus <matholv512@gmail.com>
2025-04-03 14:45:00 -0300

    test: add integration test for auth

---
+++ src/controllers/auth.test.ts
@@ -0,0 +1,92 @@
+import 'dotenv/config'
+import mongoose from 'mongoose'
+import express, { Express, Router } from 'express'
+import { login, logout } from './auth.ts'
+import { errorHandler } from '../middlewares/error-handler.ts'
+import request from 'supertest'
+import { createUser } from '../utils/create-user.ts'
+import { verifyToken } from '../utils/verify-token.ts'
+
+describe('auth service', () => {
+  let app: Express
+  const user = createUser()
+  beforeAll(async () => {
+    await mongoose.connect(process.env.URI_TEST)
+    app = express()
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+    const router = Router()
+    router.post('/auth', login)
+    router.delete('/auth', logout)
+    app.use(router)
+
+    app.use(errorHandler)
+  })
+
+  afterAll(async () => {
+    await mongoose.connection.close()
+  })
+
+  it('should authenticate a valid user', async () => {
+    const { email, password } = user
+
+    const response = await request(app).post('/auth').send({ email, password })
+    expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty('message')
+    expect(response.body.message).toBe('Successfully logged in.')
+    expect(response.headers).toHaveProperty('set-cookie')
+    const token = response.headers['set-cookie'][0]
+      .split('token=')[1]
+      .split(';')[0]
+    expect(verifyToken(token)).toMatchObject({
+      email: expect.any(String),
+      exp: expect.any(Number),
+      iat: expect.any(Number),
+      id: expect.any(String),
+      name: expect.any(String),
+      role: expect.any(String),
+    })
+  })
+
+  it('should return an error when we try to authenticate an unregistered user', async () => {
+    const response = await request(app)
+      .post('/auth')
+      .send({ email: 'carlos888@gmail.com', password: '123456' })
+
+    expect(response.status).toBe(401)
+    expect(response.body).toHaveProperty('message')
+    expect(response.body.message).toBe('Invalid e-mail or password.')
+    expect(response.headers).not.toHaveProperty('set-cookie')
+  })
+
+  it('should return an error when we try to authenticate a user with an wrong password', async () => {
+    const { email } = createUser()
+    const response = await request(app)
+      .post('/auth')
+      .send({ email, password: '218941874' })
+
+    expect(response.status).toBe(401)
+    expect(response.body).toHaveProperty('message')
+    expect(response.body.message).toBe('Invalid e-mail or password.')
+    expect(response.headers).not.toHaveProperty('set-cookie')
+  })
+
+  it('should disconnect a logged in user', async () => {
+    const { email, password } = user
+
+    const loginResponse = await request(app)
+      .post('/auth')
+      .send({ email, password })
+    expect(loginResponse.headers).toHaveProperty('set-cookie')
+
+    const logoutResponse = await request(app).delete('/auth')
+    expect(logoutResponse.status).toBe(200)
+    expect(logoutResponse.body).toHaveProperty('message')
+    expect(logoutResponse.body.message).toBe('Successfully logged out.')
+    expect(logoutResponse.headers).toHaveProperty('set-cookie')
+
+    const isEmptyToken =
+      logoutResponse.headers['set-cookie'][0].includes('token=;')
+    expect(isEmptyToken).toBe(true)
+  })
+})
